17198f5cd56e630e475cbee5a5b48f7728a99567
arrazyfathan <arrazy.rabbani266@gmail.com>
2022-11-16 10:04:57 +0700

    Chapter 5: Done

--- app/src/main/java/com/arrazyfathan/reactiveprogramming/operator/Operator.kt
+++ app/src/main/java/com/arrazyfathan/reactiveprogramming/operator/Operator.kt
@@ -1,6 +1,7 @@
 package com.arrazyfathan.reactiveprogramming.operator
 
 import com.arrazyfathan.reactiveprogramming.utils.exampleOf
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.subscribeBy
 import io.reactivex.rxjava3.subjects.PublishSubject
@@ -57,6 +58,190 @@ fun operatorElementAt() {
     }
 }
 
+/*
+* Filter Element
+* Filter takes a predicate lambda, which applies to each element. allowing through only those element for which the predicate resolve true
+* */
+fun operatorFilter() {
+    exampleOf("Filter") {
+        val subscription = CompositeDisposable()
+        subscription.add(
+            Observable.fromIterable(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+                .filter { number ->
+                    number % 2 == 0
+                }
+                .subscribeBy {
+                    println(it)
+                }
+        )
+    }
+}
+
+/*
+* Skipping Operator
+* Skip operator allows you to ignore from the first to the number you pass as its parameters
+* */
+fun operatorSkip() {
+    exampleOf("Skip") {
+        val compositeDisposable = CompositeDisposable()
+        compositeDisposable.add(
+            Observable.just("A", "B", "C", "D", "E", "F", "G")
+                .skip(3)
+                .subscribeBy {
+                    println(it)
+                }
+        )
+    }
+}
+
+/*
+* SkipWhile Operator
+* Like filter skipWhile lets you include a predicate to determine what should be skipped.
+* */
+fun operatorSkipWhile() {
+    exampleOf("Skip While") {
+        val subscription = CompositeDisposable()
+        subscription.add(
+            Observable.just(2, 3, 2, 3, 4)
+                .skipWhile { number ->
+                    number % 2 == 0
+                }
+                .subscribeBy { result ->
+                    println(result)
+                }
+        )
+    }
+}
+
+/*
+* Skip Until Operator
+* Will keep skipping elements from the source observable ( the one you are subscibing to ) until some other trigger observable emits.
+* */
+fun operatorSkipUntil() {
+    exampleOf("Skip Until") {
+        val subscription = CompositeDisposable()
+        val subject = PublishSubject.create<String>()
+        val trigger = PublishSubject.create<String>()
+
+        subscription.add(
+            subject.skipUntil(trigger)
+                .subscribeBy {
+                    println(it)
+                }
+        )
+
+        subject.onNext("A")
+        subject.onNext("B")
+        trigger.onNext("X")
+        subject.onNext("C")
+    }
+}
+
+/*
+* Taking Operator
+* Taking is the opposite of skipping. When you want to only take certain element, RxJava has you covered.
+* */
+fun operatorTake() {
+    exampleOf("Take Operator") {
+        val subscription = CompositeDisposable()
+        subscription.add(
+            Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .take(3)
+                .subscribeBy {
+                    println(it)
+                }
+        )
+    }
+}
+
+/*
+* TakeWhile
+* takeWhile works simililary to skipWhile except youre taking instead of skipping.
+* */
+fun operatorTakingWhile() {
+    exampleOf("takeWhile") {
+        val subscription = CompositeDisposable()
+        subscription.add(
+            Observable.fromIterable(listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1))
+                .takeWhile { number ->
+                    number < 5
+                }
+                .subscribeBy {
+                    println(it)
+                }
+        )
+    }
+}
+
+/*
+* takeUntil operator
+* */
+fun operatorTakeUntil() {
+    exampleOf("Take Until") {
+        val subscription = CompositeDisposable()
+        val subject = PublishSubject.create<String>()
+        val trigger = PublishSubject.create<String>()
+
+        subscription.add(
+            subject.takeUntil(trigger)
+                .subscribeBy {
+                    println(it)
+                }
+        )
+
+        subject.onNext("1")
+        subject.onNext("2")
+        subject.onNext("3")
+        subject.onNext("3")
+        trigger.onNext("X")
+        subject.onNext("3")
+        subject.onNext("3")
+    }
+}
+
+/*
+* Distinct Until Change Operator
+* prevent duplicate items one after another form getting through. Distinct until change only prevents duplicate that right next to each other.
+* disticntUntilChange only prevent contiguous duplicates.
+* */
+fun operatorDistinctUntilChange() {
+    exampleOf("distinctUntilChange") {
+        val subscriptions = CompositeDisposable()
+        subscriptions.add(
+            Observable.just("Dog", "Cat", "Cat", "Dog")
+                .distinctUntilChanged()
+                .subscribeBy {
+                    println(it)
+                }
+        )
+    }
+}
+
+/*
+* DistictUntilChange Predicate
+* */
+fun operatorDistinctUntilChangePredicate() {
+    exampleOf("DistinctUntilChangePredicate") {
+        val subs = CompositeDisposable()
+        subs.add(
+            Observable.just("ABC", "BCD", "CDE", "FGH", "IJK", "JKL", "LMN")
+                .distinctUntilChanged { first, second ->
+                    second.any { it in first }
+                }
+                .subscribeBy {
+                    println(it)
+                }
+        )
+    }
+}
+
+/*                  Key Points
+*
+* Ignoring : operator like ignoreElement , elementAt, and filter let you remove certain element from an observable stream
+* Skipping : operators let you skip certain elements and then begin emitting
+* Conversely : taking operators let you take certain elements and then stop emitting
+* Distinct : operators let you prevent duplicates from being emitted back-to-back in observable stream.
+* */
 fun main() {
-    operatorElementAt()
+    operatorDistinctUntilChangePredicate()
 }
